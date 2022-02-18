c26adf3e1ccaaf3803a70ca3c9cad1ff9e9fd5e6
Nils <nils.weichler@hotmail.de>
2022-02-18 01:23:02 +0100

    ausgelagert

--- pages/_app.js
+++ pages/_app.js
@@ -2,32 +2,15 @@ import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 import { Toaster } from 'react-hot-toast'
 import {UserContext} from "../lib/context";
-import {useEffect, useState} from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import {auth, firestore} from "../lib/firebase";
+
+import { useUserData } from '../lib/hooks';
 
 function MyApp({ Component, pageProps }) {
-  const [user] = useAuthState(auth);
-  const [company, setCompany] = useState(null);
 
-  useEffect(() => {
-    let unsubscribe;
-
-    if(user) {
-      const ref= firestore.collection('users').doc(user.uid);
-      unsubscribe = ref.onSnapshot((doc) => {
-        setCompany(doc.data()?.company)
-      });
-    } else {
-      setCompany(null);
-    }
-
-    return unsubscribe;
-
-  }, [user]);
+  const userData = useUserData();
 
   return (
-  <UserContext.Provider value={{ user, company }}>
+  <UserContext.Provider value={userData}>
     <Navbar></Navbar>
     <Component {...pageProps} />
     <Toaster></Toaster>
