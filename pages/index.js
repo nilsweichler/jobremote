0f668be7fe2acbabb4690956e716911a7723b5a5
Nils <nils.weichler@hotmail.de>
2022-02-19 19:31:14 +0100

    hero-section

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import Loader from '../components/loader'
 
-import toast from 'react-hot-toast'
 
 export default function Home() {
   return (
-    <main>
-      <button onClick={() => toast.error('Toast Works!')}>
-        Press Me!
-      </button>
-    </main>
+      <section className={styles.heroSection}>
+          <div className={styles.heroText}>
+              <h1>Entdecke hunderte Remote Jobs</h1>
+              <p>Suchen Sie nach Hunderten von Jobs und finden Sie <br/>
+                  einen Job, der zu Ihnen passt! <br/>
+                  Oder erstellen Sie einen Job GRATIS.</p>
+              <button className={styles.heroButton}>Poste einen Job</button>
+          </div>
+      </section>
   )
 }
